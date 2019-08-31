fe603097daac62f7734d5ce0cfad0def072a2151
Nicolás Vega <nicolasvega@gmail.com>
2019-08-31 17:19:50 -0300

    Saco la configuracion de la vista ya que no la voy a usar.

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ var weatherRouter = require('./routes/weather');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
